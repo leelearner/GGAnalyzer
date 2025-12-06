2ae0c91ec14addc405220a4607373c384f087ddd
Xingyu Li <xl5936@nyu.edu>
2025-12-06 18:28:00 -0500

    Write data to matches and teams

--- backend/src/main/java/com/gganalyzer/repository/LeagueRepository.java
+++ backend/src/main/java/com/gganalyzer/repository/LeagueRepository.java
@@ -3,7 +3,9 @@ package com.gganalyzer.repository;
 import com.gganalyzer.model.League;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface LeagueRepository extends JpaRepository<League, Long> {
+    Optional<League> findByName(String name);
 }
